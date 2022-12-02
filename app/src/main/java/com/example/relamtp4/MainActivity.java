68bbdddf85282cc7595c9ebf4c5ec927b176beca
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-02 12:24:03 +0100

    add product

--- app/src/main/java/com/example/relamtp4/MainActivity.java
+++ app/src/main/java/com/example/relamtp4/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import io.realm.Realm;
 
+public class MainActivity extends AppCompatActivity {
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(this);
+
+        realm = Realm.getDefaultInstance();
     }
 }
