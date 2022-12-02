1003ca199dce437613e3fb96eff4e757e8d60ebb
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-02 17:58:54 +0100

    update product

--- app/src/main/java/com/example/relamtp4/adpaters/ProductsAdapter.java
+++ app/src/main/java/com/example/relamtp4/adpaters/ProductsAdapter.java
@@ -1,8 +1,10 @@
 package com.example.relamtp4.adpaters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.relamtp4.R;
 import com.example.relamtp4.models.Product;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
@@ -44,18 +47,41 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.MyView
 
         //add click listener to the card view
         holder.itemView.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(context);
+            dialog.setContentView(R.layout.update_delete_product_dialog);
+            dialog.setCancelable(true);
+            dialog.show();
 
+            //get the views
+            TextInputEditText name = dialog.findViewById(R.id.product_name_input);
+            TextInputEditText price = dialog.findViewById(R.id.product_price_input);
+            Button updateButton = dialog.findViewById(R.id.update_product_button);
+            Button deleteButton = dialog.findViewById(R.id.delete_product_button);
 
+            //set the views
+            name.setText(product.getName());
+            price.setText(String.valueOf(product.getPrice()));
 
-//            Snackbar.make(v, "Are you sure you want to delete this product?", Snackbar.LENGTH_LONG)
-//                    .setAction("Yup", v1 -> {
-//                        //delete product from realm
-//                        realm.executeTransaction(realm -> {
-//                            Product productToDelete = realm.where(Product.class).equalTo("id", product.getId()).findFirst();
-//                            productToDelete.deleteFromRealm();
-//                            notifyItemRemoved(position);
-//                        });
-//                    }).show();
+            //add click listener to delete button
+            deleteButton.setOnClickListener(v1 -> {
+                realm.executeTransaction(realm -> {
+                    Product productToDelete = realm.where(Product.class).equalTo("id", product.getId()).findFirst();
+                    productToDelete.deleteFromRealm();
+                    notifyItemRemoved(position);
+                    dialog.dismiss();
+                });
+            });
+
+            //add click listener to update button
+            updateButton.setOnClickListener(v12 -> {
+                realm.executeTransaction(realm -> {
+                    Product productToUpdate = realm.where(Product.class).equalTo("id", product.getId()).findFirst();
+                    productToUpdate.setName(name.getText().toString());
+                    productToUpdate.setPrice(Double.parseDouble(price.getText().toString()));
+                    notifyItemChanged(position);
+                    dialog.dismiss();
+                });
+            });
         });
     }
 
