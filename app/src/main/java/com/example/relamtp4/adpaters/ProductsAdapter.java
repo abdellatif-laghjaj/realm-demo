5e6fd043262d8a6a73012407a888028f7051a81b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-02 16:21:51 +0100

    update ui

--- app/src/main/java/com/example/relamtp4/adpaters/ProductsAdapter.java
+++ app/src/main/java/com/example/relamtp4/adpaters/ProductsAdapter.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.relamtp4.R;
 import com.example.relamtp4.models.Product;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -35,6 +36,21 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.MyView
         Product product = products.get(position);
         holder.name.setText(product.getName());
         holder.price.setText(String.valueOf(product.getPrice()));
+
+        //add click listener to the card view
+        holder.itemView.setOnClickListener(v -> {
+            Snackbar.make(v, "Are you sure you want to delete this product?", Snackbar.LENGTH_LONG)
+                    .setAction("Yup", v1 -> {
+                        //delete product from realm
+//                        realm.executeTransaction(realm -> {
+//                            product.deleteFromRealm();
+//                        });
+                        //remove product from the list
+                        products.remove(position);
+                        //notify adapter that the data has changed
+                        notifyDataSetChanged();
+                    }).show();
+        });
     }
 
     @Override
